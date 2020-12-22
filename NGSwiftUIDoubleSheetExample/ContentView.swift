b243c0d8a901587f70ffdf990cd60cc6d187177a
Noah Gilmore <noah.w.gilmore@gmail.com>
2020-12-22 09:32:48 -0800

    Add code demonstrating the issue

--- NGSwiftUIDoubleSheetExample/ContentView.swift
+++ NGSwiftUIDoubleSheetExample/ContentView.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isPresenting1 = false
+    @State var isPresenting2 = false
+
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        VStack {
+            Button("Present Sheet 1") {
+                self.isPresenting1 = true
+            }
+            Button("Present Sheet 2") {
+                self.isPresenting2 = true
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .sheet(isPresented: self.$isPresenting1, content: {
+            Button("Dismiss") {
+                self.isPresenting1 = false
+            }
+            .frame(width: 200, height: 200).background(Color.red)
+        })
+        .sheet(isPresented: self.$isPresenting2, content: {
+            Button("Dismiss") {
+                self.isPresenting2 = false
+            }
+            .frame(width: 200, height: 200).background(Color.red)
+        })
     }
 }
 
